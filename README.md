943f5d8960a8bbf24a27b220a2990e84620198ce
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-01-22 00:17:34 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">React Redux App</h1><br/>
+
+## Dependencies
+
+- Bootstrap: npm i redux react-redux redux-thunk
+
+## Links
+
+- Rest API: https://jsonplaceholder.typicode.com/
---
+++ src/components/PostForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class PostForm extends Component {
+  state = {
+    title: '',
+    body: ''
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
+
+  render() {
+    const { title, body } = this.state;
+
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={title}
+            />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="body">Body:</label>
+            <br />
+            <textarea name="body" onChange={this.onChange} value={body} />
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
